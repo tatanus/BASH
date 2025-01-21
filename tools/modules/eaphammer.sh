0b58c46593797b5d62390da379523722ff8d4463
tatanus <adam.compton@trustedsec.com>
2025-01-21 11:44:08 -0500

    moved eaphammer from lib to tools modules dir

--- tools/modules/eaphammer.sh
+++ tools/modules/eaphammer.sh
@@ -24,7 +24,18 @@ function install_eaphammer() {
     _Install_Git_Python_Tool "${TOOL_NAME}" "${GIT_URL}" false "${REQUIREMENTS_FILE}" "${PIP_INSTALLS[@]}"
 
     # more setup
-    _Pushd eaphammer
+    # Determine DIRECTORY_NAME from GIT_URL
+    local DIRECTORY_NAME="${GIT_URL}"
+
+    # Remove .git suffix if it exists
+    if [[ "${DIRECTORY_NAME}" == *.git ]]; then
+        DIRECTORY_NAME=${DIRECTORY_NAME%.git}
+    fi
+
+    # remove everything  except after the last \
+    DIRECTORY_NAME="${DIRECTORY_NAME##*/}"
+
+    _Pushd "${TOOLS_DIR}/${DIRECTORY_NAME}"
     source ./venv/bin/activate
     sed -i 's/\._core\././' cert_wizard/cert_utils.py
     echo 'y' | ./kali-setup
