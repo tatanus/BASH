2c200bacd115a1c0ae7c5cb50f4230e52c83e494
Adam Compton <adam.compton@trustedsec.com>
2025-01-22 19:46:28 -0500

    fixed formating

--- tests/shellcheck_test.sh
+++ tests/shellcheck_test.sh
@@ -9,5 +9,4 @@ set -euo pipefail
 #find . -type f -name "*.sh" -exec shellcheck -s bash -a -x {} \;
 find . -type f -name "*.sh" -exec shellcheck --shell=bash --external-sources -x -S style -f gcc -e SC1090 -e SC1091 -e SC2034 -e SC2181 {} \;
 
-
 echo "ShellCheck passed!"
