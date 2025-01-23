3e916c83eee37a1e24b281623b34b25c9d8d85cb
Adam Compton <adam.compton@trustedsec.com>
2025-01-22 19:44:16 -0500

    updated calls to shellcheck

--- tests/shellcheck_test.sh
+++ tests/shellcheck_test.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 
 # Find all shell scripts and run ShellCheck
 #find . -type f -name "*.sh" -exec shellcheck -s bash -a -x -f diff {} \;
-find . -type f -name "*.sh" -exec shellcheck -s bash -a -x {} \;
 #find . -type f -name "*.sh" -exec shellcheck -s bash -a -e SC1090 -e SC1091 -e SC2034 -e SC2181 {} \;
+#find . -type f -name "*.sh" -exec shellcheck -s bash -a -x {} \;
+find . -type f -name "*.sh" -exec shellcheck --shell=bash --external-sources -x -S style -f gcc -e SC1090 -e SC1091 -e SC2034 -e SC2181 {} \;
+
 
 echo "ShellCheck passed!"
